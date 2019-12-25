0c4393dcf87e099762a291887dbdc983add2fdd7
Насибуллин Тимур Зуфарович <nasibullin.t.zu@sberbank.ru>
2019-12-25 12:33:33 +0300

    deleted div

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,26 +1,25 @@
 import React, { useState } from "react"
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from "reactstrap"
+import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from "reactstrap"
+import Container from "reactstrap/es/Container"
 import logo from "../logo.svg"
 
 export default ( { children } ) => {
     const [collapsed, setCollapsed] = useState( true )
     const toggleNavbar = () => setCollapsed( !collapsed )
 
-    return <div>
-        <Container>
-            <NavbarBrand href="/"><img src={ logo } width="30" height="30" alt="logo"/>MERN-Stack Todo App</NavbarBrand>
-            <NavbarToggler onClick={ toggleNavbar } className="mr-2"/>
-            <Collapse isOpen={ !collapsed } navbar>
-                <Navbar color="faded" dark>
-                    <NavItem>
-                        <NavLink href="/">Todos</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/create">Create Todo</NavLink>
-                    </NavItem>
-                </Navbar>
-            </Collapse>
-            { children }
-        </Container>
-    </div>
+    return <Container>
+        <NavbarBrand href="/"><img src={ logo } width="30" height="30" alt="logo"/>MERN-Stack Todo App</NavbarBrand>
+        <NavbarToggler onClick={ toggleNavbar } className="mr-2"/>
+        <Collapse isOpen={ !collapsed } navbar>
+            <Navbar color="faded" dark>
+                <NavItem>
+                    <NavLink href="/">Todos</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="/create">Create Todo</NavLink>
+                </NavItem>
+            </Navbar>
+        </Collapse>
+        { children }
+    </Container>
 }
